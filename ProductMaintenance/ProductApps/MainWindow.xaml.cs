d5825364c3d59dfeaa7f36da3a197a9c9cf4f997
jacqueline Summers <jacqueline.summers@student.tafesa.edu.au>
2024-04-08 12:53:46 +0930

    Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -33,9 +33,9 @@ namespace ProductApps
             {
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
-                //cProduct.calTotalDeliveryPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
-                totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25);
+                totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25m);
+                totalWrapChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25m + 5m);
             }
             catch (FormatException)
             {
