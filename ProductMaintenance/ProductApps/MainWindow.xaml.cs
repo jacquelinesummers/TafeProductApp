9793bd7f60e716b623f8739965addf5d12cf4d32
jacqueline Summers <jacqueline.summers@student.tafesa.edu.au>
2024-04-08 12:36:33 +0930

    Added the delivery charge $25

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -33,9 +33,9 @@ namespace ProductApps
             {
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
-                cProduct.calTotalDeliveryPayment();
+                //cProduct.calTotalDeliveryPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
-                totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalDeliveryPayment);
+                totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25);
             }
             catch (FormatException)
             {
